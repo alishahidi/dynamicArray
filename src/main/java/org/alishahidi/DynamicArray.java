6395581c1adcad66be0d6b2dc67f2435713f1cc5
Ali Shahidi <alishahidi1376@gmail.com>
2023-07-31 12:03:49 +0330

    complete DynamicArray.java file functionality

--- src/main/java/org/alishahidi/DynamicArray.java
+++ src/main/java/org/alishahidi/DynamicArray.java
@@ -1,7 +1,113 @@
 package org.alishahidi;
 
+import java.util.stream.IntStream;
+
 public class DynamicArray {
     private int size;
     private int capacity = 10;
     private Object[] array;
+
+    public DynamicArray() {
+        array = new Object[capacity];
+    }
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        array = new Object[capacity];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Object[] getArray() {
+        return array;
+    }
+
+    public void add(Object data) {
+        if (size >= capacity) {
+            grow();
+        }
+        array[size] = data;
+        size++;
+    }
+
+    public void insert(int index, Object data) {
+        if (size >= capacity) {
+            grow();
+        }
+        IntStream.iterate(size, i -> i > index, i -> i - 1).forEach(i -> array[i] = array[i - 1]);
+        array[index] = data;
+        size++;
+    }
+
+    public void delete(Object data) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == data) {
+                for (int j = 0; j < (size - i - 1); j++) {
+                    array[i + j] = array[i + j + 1];
+                }
+                array[size - 1] = null;
+                size--;
+                if (size <= (int) (capacity / 3)) {
+                    shrink();
+                }
+                break;
+            }
+        }
+    }
+
+    public int search(Object data) {
+        for (int i = 0; i < size; i++) {
+            if(array[i] == data){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void grow() {
+        int newCapacity = (int) (capacity * 2);
+        Object[] newArray = new Object[newCapacity];
+        for(int i = 0; i < size; i++){
+            newArray[i] = array[i];
+        }
+        capacity = newCapacity;
+        array = newArray;
+     }
+
+    public void shrink() {
+        int newCapacity = (int) (capacity / 2);
+        Object[] newArray = new Object[newCapacity];
+        for(int i = 0; i < size; i++){
+            newArray[i] = array[i];
+        }
+        capacity = newCapacity;
+        array = newArray;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            string.append(array[i]).append(", ");
+        }
+        if (!string.toString().equals("[")) {
+            string.setLength(string.length() - 2);
+        }
+        string.append("]");
+        return string.toString();
+    }
 }
--- src/main/java/org/alishahidi/Main.java
+++ src/main/java/org/alishahidi/Main.java
@@ -2,6 +2,24 @@ package org.alishahidi;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DynamicArray dynamicArray = new DynamicArray(5);
+        dynamicArray.add(5);
+        dynamicArray.insert(0, "Hi");
+        dynamicArray.add("Ali");
+        dynamicArray.add("Melika");
+        dynamicArray.add("Mohammad");
+        dynamicArray.add("Mohammad");
+        dynamicArray.add("Mohammad");
+        dynamicArray.add("Mohammad");
+        dynamicArray.add("Mohammad");
+        dynamicArray.add("Mohammad");
+        dynamicArray.add("Mohammad");
+        dynamicArray.delete("Melika");
+        System.out.println(dynamicArray.search("Ali"));;
+
+        System.out.println(dynamicArray);
+        System.out.println(dynamicArray.isEmpty());
+        System.out.println(dynamicArray.getCapacity());
+        System.out.println(dynamicArray.getSize());
     }
 }
